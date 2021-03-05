764861213b9ca9127f321aa06532a8a3032aaf6a
wakils <villads111@gmail.com>
2021-03-06 00:21:42 +0100

    popup build

--- src/popup/TitleStuffyDuffy.svelte
+++ src/popup/TitleStuffyDuffy.svelte
@@ -1,5 +1,5 @@
 <main>
   <h1>Buff Currency</h1>
-	<h2>Choose a currency</h2>
-	<p>Created by wakils.com & uwuwuwuwu lignus</p>
+	<h4>Choose a currency</h4>
+	<p>Authors: wakils & linnnus</p>
 </main>
