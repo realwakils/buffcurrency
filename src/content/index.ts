742a3a61af283dddb7f8135fa042af66e4099a67
wakils <villads111@gmail.com>
2021-03-08 21:15:13 +0100

    content script

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,8 +1,22 @@
 import { fetchData } from "../utils/fetchData";
 
+let currentNode;
+
 (async () => {
 	const [data, currentCurrency] = await fetchData();
 	const rate = data.rates[currentCurrency];
 
-	console.log("This is where we'd replace using: ", rate);
+	setTimeout(() => {
+		const tags = [...document.querySelectorAll('strong.f_Strong')];
+		for (let i = 0; i < tags.length; i++) {
+			currentNode = tags[i];
+			try {
+				const priceCNY = parseFloat(currentNode.innerText.replace('¥ ', ''));
+				const priceConverted = priceCNY * rate;
+				currentNode.innerText = `${priceConverted.toFixed(2)} ${currentCurrency}`;
+			} catch (e) {}
+		}
+	}, 1500)
+	
+
 })();
