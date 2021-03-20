04a553070119024b982cdff0d84521eaaa584789
wakils <villads111@gmail.com>
2021-03-20 21:37:36 +0100

    mutationobserver, regex pattern replace and more. broke some stuff tho

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,22 +1,17 @@
 import { fetchData } from "../utils/fetchData";
-
-let currentNode;
+import * as Observer from './MutationObserver';
 
 (async () => {
 	const [data, currentCurrency] = await fetchData();
 	const rate = data.rates[currentCurrency];
 
-	setTimeout(() => {
-		const tags = [...document.querySelectorAll('strong.f_Strong')];
-		for (let i = 0; i < tags.length; i++) {
-			currentNode = tags[i];
-			try {
-				const priceCNY = parseFloat(currentNode.innerText.replace('¥ ', ''));
+	Observer.startObserver((thingyBobs: Array<any>) => {
+		thingyBobs.forEach(thingyBob => {
+			thingyBob.innerText = thingyBob.innerText.replace(/¥ (\d+(.\d+)?)/gm, (_, g) => { // make sure reassign (example: x = x+1 and not just x+1)
+				const priceCNY = parseFloat(g); // we use group 2 (which is the price in cny) and parse it to a float
 				const priceConverted = priceCNY * rate;
-				currentNode.innerText = `${priceConverted.toFixed(2)} ${currentCurrency}`;
-			} catch (e) {}
-		}
-	}, 1500)
-	
-
+				return `${priceConverted.toFixed(2)} ${currentCurrency}`;
+			});
+		});
+	});
 })();
