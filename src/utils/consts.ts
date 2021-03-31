4e81ae9f4b88f29520027ad39a43ff1a19f360d2
wakils <villads111@gmail.com>
2021-03-31 21:18:41 +0200

    added target to targetClasses

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,2 @@
-export const targetClasses: Array<string> = ['f_Strong', 'msg-title', 'f_Normal', 'j_Goods-jump', 'active']
+export const targetClasses: Array<string> = ['f_Strong', 'msg-title', 'f_Normal', 'j_Goods-jump', 'active', 'sticker-name>div>div>span']
 export const targetClassesSelector: string = '.' + targetClasses.join(', .') + ', h5';
