d4b56332d7de5e0009c14153ccd6fef6e1787563
wakils <villads111@gmail.com>
2021-03-31 21:40:55 +0200

    added more types and cleaned up targetClasses

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,5 @@
-export const targetClasses: Array<string> = ['f_Strong', 'msg-title', 'f_Normal', 'j_Goods-jump', 'active', 'sticker-name>div>div>span']
-export const targetClassesSelector: string = '.' + targetClasses.join(', .') + ', h5';
+export const targetClasses: Array<string> = [
+	'.f_Strong', '.msg-title', '.f_Normal', '.j_Goods-jump',
+	'.active', '.sticker-name>div>div>span', 'h5', '#amount_list>span'
+];
+export const targetClassesSelector: string = targetClasses.join(', ');
