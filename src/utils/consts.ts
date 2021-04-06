da6ee3b0e72c8c29a59ccf80ab41989f9ff93af0
wakils <villads111@gmail.com>
2021-04-06 21:29:05 +0200

    fixes #7, #8 and #9

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,6 +1,7 @@
 export const targetClasses: Array<string> = [
 	'.f_Strong', '.msg-title', '.f_Normal', '.j_Goods-jump',
-	'.active', '.sticker-name>div>div>span', 'h5', '#amount_list>span'
+	'.active', '.sticker-name>div>div>span', 'h5', '#amount_list>span',
+	'.total_price', '.sale_fee', '.selling_price'
 ];
 export const targetClassesSelector: string = targetClasses.join(', ');
 export const URLS_SRC: string = "https://api.jsonbin.io/v3/b/60670390f2795c523e226eba";
