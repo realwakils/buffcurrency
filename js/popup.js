815fbb322bc2b2b61a876a0a3eedaa4354d4ca3f
linnnus <linnnus@users.noreply.github.com>
2023-03-31 16:42:12 +0200

    Clean up options page + reload button

--- js/popup.js
+++ js/popup.js
@@ -12,56 +12,93 @@ async function main() {
 		return;
 	}
 
-	// At the top, we add a small group of the most commonly used currencies.
-	const commonCurrenciesGroup = document.getElementById("commonCurrencies");
-	const commonCurrencies = ["USD", "EUR", "DKK", "SEK", "CNY"];
-	for (const currency of commonCurrencies) {
-		if (!(currency in rates)) {
-			console.error(`The currency ${currency} is missing from rates???`);
-			continue;
+	{
+		// At the top, we add a small group of the most commonly used currencies.
+		const commonCurrenciesGroup = document.getElementById("commonCurrencies");
+		const commonCurrencies = ["USD", "EUR", "DKK", "SEK", "CNY"];
+		for (const currency of commonCurrencies) {
+			if (!(currency in rates)) {
+				console.error(`The currency ${currency} is missing from rates???`);
+				continue;
+			}
+
+			const optionElement = document.createElement("OPTION");
+			optionElement.value = currency;
+			optionElement.textContent = currency;
+			optionElement.selected = currency === options.preferredCurrency;
+			commonCurrenciesGroup.appendChild(optionElement);
 		}
 
-		const optionElement = document.createElement("OPTION");
-		optionElement.value = currency;
-		optionElement.textContent = currency;
-		optionElement.selected = currency === options.preferredCurrency;
-		commonCurrenciesGroup.appendChild(optionElement);
+		// Below comes the list of all currencies.
+		const otherCurrenciesGroup = document.getElementById("otherCurrencies");
+		const otherCurrencies = Object.keys(rates).filter(c => !commonCurrencies.includes(c));
+		for (const currency of otherCurrencies) {
+			const optionElement = document.createElement("OPTION");
+			optionElement.value = currency;
+			optionElement.textContent = currency;
+			optionElement.selected = currency === options.preferredCurrency;
+			otherCurrenciesGroup.appendChild(optionElement);
+		}
+
+		const selectElement = document.getElementById("preferredCurrency");
+
+		selectElement.addEventListener("change", handleChange);
+
+		// Since currencies have been added, we can enable the select element.
+		selectElement.disabled = false;
 	}
 
-	// Below comes the list of all currencies.
-	const otherCurrenciesGroup = document.getElementById("otherCurrencies");
-	const otherCurrencies = Object.keys(rates).filter(c => !commonCurrencies.includes(c));
-	for (const currency of otherCurrencies) {
-		const optionElement = document.createElement("OPTION");
-		optionElement.value = currency;
-		optionElement.textContent = currency;
-		optionElement.selected = currency === options.preferredCurrency;
-		otherCurrenciesGroup.appendChild(optionElement);
+	{
+		const modifierElement = document.getElementById("priceModifier");
+		const modifierLabelElement = document.getElementById("priceModifierLabel");
+
+		// When the modifier element is changes value, update the label.
+		const updateLabel = () => modifierLabelElement.textContent = `Price Modifier: ${modifierElement.value}%`
+		modifierElement.addEventListener("input", updateLabel);
+		updateLabel();
+
+		modifierElement.addEventListener("change", handleChange);
+
+		modifierElement.value = options.priceModifier * 100;
+		modifierElement.disabled = false;
 	}
 
-	// Once all currencies have been added, we can enable the select element.
-	const selectElement = document.getElementById("preferredCurrency");
-	selectElement.disabled = false;
+	{
+		const refreshButton = document.getElementById("refetchRates");
 
-	const modifierLabelElement = document.getElementById("priceModifierLabel");
-	const modifierElement = document.getElementById("priceModifier");
-	const updateLabel = () => modifierLabelElement.textContent = `Price Modifier: ${modifierElement.value}%`;
-	modifierElement.addEventListener("input", updateLabel);
-	modifierElement.value = options.priceModifier * 100;
-	updateLabel();
-	modifierElement.disabled = false;
+		refreshButton.disabled = false;
 
-	const formElement = document.getElementById("optionsForm");
-	formElement.addEventListener("change", async () => {
-		options.preferredCurrency = selectElement.value;
-		options.priceModifier = Number.parseInt(modifierElement.value) / 100;
+		refreshButton.addEventListener("click", async () => {
+			const success = await chrome.runtime.sendMessage("fetch-rates");
+			if (!success) {
+				alert("Failed to update rates. See extension logs for more details.");
+			} else {
+				alert("Successfully updated rates!");
+			}
 
-		await chrome.storage.sync.set({ options });
+			// Reload all tabs such that new rates are loaded from storage.
+			await reloadAllTabs();
+		});
+	}
+}
 
-		// Notify the content script
-		const tabs = await chrome.tabs.query({ url: "*://buff.163.com/*" });
-		await Promise.all(tabs.map(t => chrome.tabs.reload(t.id)));
-	});
+async function handleChange() {
+	console.debug("Options changed");
+
+	// Save options to shared storage.
+	const options = {
+		preferredCurrency: document.getElementById("preferredCurrency").value,
+		priceModifier: Number.parseInt(document.getElementById("priceModifier").value) / 100,
+	};
+	await chrome.storage.sync.set({ options });
+
+	// Reload all tabs such that new options are loaded from storage.
+	await reloadAllTabs();
+}
+
+async function reloadAllTabs() {
+	const tabs = await chrome.tabs.query({ url: "*://buff.163.com/*" });
+	await Promise.all(tabs.map(t => chrome.tabs.reload(t.id)));
 }
 
 main();
