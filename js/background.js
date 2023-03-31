49a50a6baae00c860c0d189afa917112e5321b22
linnnus <linnnus@users.noreply.github.com>
2023-03-31 16:12:32 +0200

    Remove jsonschema dependency
    This patch removes the 3rd party dependency used for verifying the
    result of the API. The API itself is already versioned so the layout is
    guaranteed to not change (under normal curcimstances). The additional
    hassle this adds to the review process makes the sanity check not worth
    it.

--- js/background.js
+++ js/background.js
@@ -1,32 +1,11 @@
 // The background script runs in the background and is responsible for fetching the rates from an
 // external API.
 
-import { validate } from "https://esm.sh/jsonschema@1.4.1";
-
 async function fetchRates() {
 	const response = await fetch("https://open.er-api.com/v6/latest/CNY");
+	if (!response.ok)
+		throw new Error(`API request failed with code ${response.status}: ${response.StatusText}`);
 	const data = await response.json();
-
-	// Sanity check
-	const schema = {
-		type: "object",
-		properties: {
-			result: { const: "success" },
-			time_last_update_unix: { type: "integer" },
-			base_code: { const: "CNY" },
-			rates: {
-				type: "object",
-				patternProperties: {
-					"\\W{3}": { type: "number" },
-				},
-			},
-		},
-		required: ["result", "base_code", "rates", "time_last_update_utc"],
-		additionalProperties: true,
-	};
-	const { valid, errors } = validate(data, schema);
-	if (!valid)
-		throw new Error(`Recieved invalid JSON: ${errors[0].property} ${errors[0].message}`);
 
 	chrome.storage.local.set({ rates: data.rates, lastUpdate: data.time_last_update_unix }, () => {
 		if (chrome.runtime.lastError) {
